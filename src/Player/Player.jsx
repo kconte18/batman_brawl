febd6651046de3bb717f08db513c3ad48cc05ad1
kconte18 <45443589+kconte18@users.noreply.github.com>
2022-11-08 14:03:14 -0600

    Added Keydown

--- src/Player/Player.jsx
+++ src/Player/Player.jsx
@@ -1,11 +1,34 @@
+import React from 'react';
 import './Player.css';
 import batmanStand1 from '../images/sprites/batman/stand/batman-stand1.gif';
 
 function Player() {
 
+    // This handles what is pressed and how to manage it
+    const handleKeyDown = (event) => {
+        console.log(event.keyCode);
+        let player = document.getElementsByClassName("player-sprite");
+        if(event.keyCode === 68) {
+            console.log("move right");
+            player.item(0).style.left += 5;
+        }
+    };
+
+    // This adds and removes the keydown events
+    React.useEffect(() => {
+        window.addEventListener('keydown', handleKeyDown);
+    
+        // cleanup this component
+        return () => {
+          window.removeEventListener('keydown', handleKeyDown);
+        };
+      }, []);
+
+
+
     return(
         // <div className='player-container'>
-            <img className="player-sprite" src={batmanStand1} alt="byebye"></img>
+            <img className="player-sprite" src={batmanStand1} alt="player-sprite" onKeyUp={handleKeyDown}></img>
         // {/* </div> */}
     );
 }
